8e496f510bbebb65f311ca452916773ed2a4768c
RAJ <135052784+RAJSAHASWAPNO@users.noreply.github.com>
2023-06-17 11:37:55 +0600

    Add files via upload

---
+++ Assaingment oparetor.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    a=5;
+    a+=5;
+    printf("%d\n",a);
+    b=10;
+    b*=5;
+    printf("%d\n",b);
+    c=12;
+    c/=2;
+    printf("%d\n",c);
+}
